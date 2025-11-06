32282819770841e08c065f7a725dcb83f817a5ab
parthchavhan <parthjain9460@gmail.com>
2025-11-06 22:57:20 +0530

    heyy

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  webpack: (config, { dev }) => {
+  webpack: (config, { dev, isServer }) => {
     // Suppress webpack PackFileCacheStrategy warnings
     // These warnings are harmless but can be noisy
     if (dev) {
@@ -12,7 +12,23 @@ const nextConfig = {
         level: 'error',
       };
     }
+    
+    // Ensure Prisma query engine binaries are included in the build
+    if (isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+      };
+      
+      // Copy Prisma binaries to output
+      config.module = config.module || {};
+      config.module.rules = config.module.rules || [];
+    }
+    
     return config;
+  },
+  // Ensure Prisma files are included in the build
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
   },
 };
 
